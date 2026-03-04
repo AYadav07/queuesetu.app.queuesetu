2f9c94fe0d7886da7465eb5b41ddd6b90169c00d
Amresh Yadav <ayadav.1918@gmail.com>
2026-03-04 14:41:06 +0530

    Initialized repo

---
+++ src/main/java/com/queuesetu/queuesetu/QueuesetuApplication.java
@@ -0,0 +1,13 @@
+package com.queuesetu.queuesetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuesetuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueuesetuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/queuesetu/queuesetu/QueuesetuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queuesetu.queuesetu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueuesetuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
